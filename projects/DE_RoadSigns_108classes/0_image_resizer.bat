f225106f1b6b6a2be0ac5589220426eb2c6f9181
sarah-antillia <sarah@antillia.com>
2022-07-14 21:24:24 +0900

    Added a sample project DE_RoadSigns_108classes project,

---
+++ projects/DE_RoadSigns_108classes/0_image_resizer.bat
@@ -0,0 +1,2 @@
+rem 0_image_resizer.bat
+python ../../PNGImageResizer.py ./configs/0_image_resizer.conf
---
+++ projects/DE_RoadSigns_108classes/tfrecord_inspector.bat
@@ -0,0 +1,4 @@
+python ../../TFRecordInspector.py ^
+  ./TFRecord_DE_RoadSigns_108classes/train/train.tfrecord ^
+  ./TFRecord_DE_RoadSigns_108classes/label_map.pbtxt ^
+  ./Inspector/train
